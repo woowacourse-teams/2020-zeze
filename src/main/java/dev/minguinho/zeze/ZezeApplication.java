a6fa9e7d9c23ae64d400cb2244c3875d1d6c80da
Woonjang Ahn <woonjangahn@gmail.com>
2020-07-13 17:21:18 +0900

    build: setup project

---
+++ src/main/java/dev/minguinho/zeze/ZezeApplication.java
@@ -0,0 +1,13 @@
+package dev.minguinho.zeze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZezeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZezeApplication.class, args);
+    }
+
+}
