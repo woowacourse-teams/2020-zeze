2c39d8fb6422025178a7fe2ac50d9c6e0184e1a1
Woonjang Ahn <woonjangahn@gmail.com>
2020-07-15 13:35:54 +0900

    chore: apply javascript convention based on naver javascript style guide and typescript eslint

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,6 @@
-/// <reference types="react-scripts" />
+// / <reference types="react-scripts" />
+declare module "*.svg" {
+    const content: string;
+    // noinspection JSDuplicatedDeclaration
+    export default content;
+}
