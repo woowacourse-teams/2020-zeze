5a6506c5c233e5806639a38ddea7fda126c06b92
Woonjang Ahn <woonjangahn@gmail.com>
2020-07-16 16:39:30 +0900

    fix: add Error component for fallback within BrowserRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import Home from "./components/home";
+import Me from "./components/me";
 import Editor from "./components/editor";
 import Archive from "./components/archive";
-import Me from "./components/me";
-import {TodoApi} from "./api";
+import Error from "./components/error";
 
 const App: React.FC = () => (
   <BrowserRouter>
@@ -14,6 +14,7 @@ const App: React.FC = () => (
       <Route path="/me" component={Me} />
       <Route path="/editor" component={Editor} />
       <Route path="/archive" component={Archive} />
+      <Route component={Error} />
     </Switch>
   </BrowserRouter>
 );
