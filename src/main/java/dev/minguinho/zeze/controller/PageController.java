94761da6f38864605b5481cd24b484a068f67c2e
hodol <woonjangahn@gmail.com>
2020-07-17 11:47:39 +0900

    Fix/#32 react router as default router (#33)
    * fix: add PageController for redirecting requests
    
    * fix: add Error component for fallback within BrowserRouter
    
    * refactor: change import strategy for components used in routes

---
+++ src/main/java/dev/minguinho/zeze/controller/PageController.java
@@ -0,0 +1,16 @@
+package dev.minguinho.zeze.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    /*
+     * Redirects all routes to client except: '/', '/index.html', '/api', '/api/**'
+     */
+    @RequestMapping(value = "{_:^(?!index\\.html|api).*$}")
+    public String redirectApi() {
+        return "forward:/";
+    }
+}
