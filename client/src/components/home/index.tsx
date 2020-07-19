f8d4a1135669dae1db13065c3af65b8239205c44
Woonjang Ahn <woonjangahn@gmail.com>
2020-07-19 14:29:02 +0900

    feat: add fullscreen component sample usage

--- client/src/components/home/index.tsx
+++ client/src/components/home/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Layout from "../common/Layout";
+import FullScreenMode from "../common/FullScreenMode";
+import {sampleMarkdown} from "../../utils/fixtures";
 
 const Home: React.FC = () => (
   <Layout>
     <h1>Home</h1>
+
+    {/* FullScreenMode Sample*/}
+    <FullScreenMode
+      content={sampleMarkdown.content}
+      delimiter={sampleMarkdown.delimiter}
+    />
+
   </Layout>
 );
 
