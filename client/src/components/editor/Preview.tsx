03d46b6944dfbc2dfa58441d2f08bd776e57b465
YerinCho <58128797+YerinCho@users.noreply.github.com>
2020-07-22 14:01:08 +0900

    Feat/#8 preview (#45)
    * feat: add marked and @types/marked
    
    * feat: add Preview component
    
    * feat: add EditMode component
    
    * refactor: remove inputbox
    
    * feat: create text state
    
    * refactor: move marked.setOptions function

---
+++ client/src/components/editor/Preview.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import marked from "marked";
+
+interface IProps {
+  content: string
+}
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+});
+
+const Preview: React.FC<IProps> = ({content}) => (
+  <div>
+    <div dangerouslySetInnerHTML={{__html: marked(content)}}/>
+  </div>
+);
+
+export default Preview;
--- client/src/components/editor/index.tsx
+++ client/src/components/editor/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "../common/Layout";
+import EditMode from "./EditMode";
 
 const Editor: React.FC = () => (
   <Layout>
     <h1>Editor</h1>
+    <EditMode/>
   </Layout>
 );
 
