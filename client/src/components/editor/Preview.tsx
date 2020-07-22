b80978c01ecfc80a093e7aa80c0fac8c8867502e
lalize <printf@kakao.com>
2020-07-22 14:19:02 +0900

    feat: apply custom renderer to preview component

--- client/src/components/editor/Preview.tsx
+++ client/src/components/editor/Preview.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import marked from "marked";
+import Renderer from "../../utils/markdown/renderer"
+import youtube from "../../utils/markdown/replacers/youtube"
 
 interface IProps {
   content: string
 }
 
+const renderer = new Renderer().setCodeReplacer("youtube", youtube)
+
 marked.setOptions({
-  renderer: new marked.Renderer(),
+  renderer,
 });
 
 const Preview: React.FC<IProps> = ({content}) => (
