fceff6cb42ede437e0e890c9c4f3afa263b658ee
jaeju.jang <jjj0611@gmail.com>
2020-07-23 19:44:07 +0900

    feat: add jwt token provider to issue user token
    	- make UserService to save User and UserResource
    	- make TokenService to issue token with JwtTokenProvider
    	- configure jwt token and untrack the yml file

--- src/main/java/dev/minguinho/zeze/domain/auth/service/UserService.java
+++ src/main/java/dev/minguinho/zeze/domain/auth/service/UserService.java
@@ -2,7 +2,10 @@ package dev.minguinho.zeze.domain.auth.service;
 
 import org.springframework.stereotype.Service;
 
+import dev.minguinho.zeze.domain.auth.model.Social;
+import dev.minguinho.zeze.domain.auth.model.User;
 import dev.minguinho.zeze.domain.auth.model.UserRepository;
+import dev.minguinho.zeze.domain.auth.service.socialfetcher.resourcefetcher.dto.response.SocialResourceResponseDto;
 import dev.minguinho.zeze.domain.resource.model.UserResourceRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -12,4 +15,15 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserResourceRepository userResourceRepository;
+
+    public User save(SocialResourceResponseDto response, Social.Provider provider) {
+        Social social = Social.builder()
+            .provider(provider)
+            .socialId(response.getSocialId())
+            .build();
+        User user = userRepository.findBySocial(social)
+            .orElse(userRepository.save(response.createUser(provider)));
+        userResourceRepository.save(response.createUserResource(user.getId()));
+        return user;
+    }
 }
