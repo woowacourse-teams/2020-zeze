746b0a615e6bd7f8088b34fef736d851423b7062
hodol <woonjangahn@gmail.com>
2020-07-23 17:08:00 +0900

    Feat/#13 presentation mode (#35)
    * dependencies: add fscreen, @types/fscreen
    
    * chore: add fixtures and constants
    
    * feat: add full screen mode component
    
    * fix: add global css to solve bugs in fullscreen mode in safari and chrome
    
    * feat: add fullscreen component sample usage
    
    * dependencies: add fscreen, @types/fscreen
    
    * refactor: change FullScreenMode related css, handleKeyDown()

---
+++ client/src/utils/fixtures.ts
@@ -0,0 +1,25 @@
+export const sampleMarkdown = {
+  content: "---\n" +
+    "created_at: 2020-07-12\n" +
+    "author: Mika\n" +
+    "pagination: false\n" +
+    "title: introducing limetree\n" +
+    "---\n" +
+    "\n" +
+    "# Minimal Presentation\n" +
+    "## With Markdown\n" +
+    "\n" +
+    "> Sometimes we only need a simple and light slides.\n" +
+    "> Why not make them with markdown?\n" +
+    "\n" +
+    "---\n" +
+    "\n" +
+    "# Features\n" +
+    "\n" +
+    "- Supports all GFM styled markdown\n" +
+    "- Embedded Charts, Images, Youtube videos\n" +
+    "- Everything pixel perfect\n" +
+    "\n" +
+    "---",
+  delimiter: "---",
+};
