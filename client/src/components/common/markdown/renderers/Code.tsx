64c6d8fe00a9b8867ab9a0132d6712b275b848a1
lalize <printf@kakao.com>
2020-07-26 20:26:04 +0900

    chore: rename lowercase to uppercase

---
+++ client/src/components/common/markdown/renderers/Code.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Youtube from "../extensions/Youtube";
+
+interface IProps {
+  language: string | undefined,
+  value: string
+}
+
+const Code: React.FC<IProps> = ({ language, value }) => {
+  if (language === "youtube") {
+    return (<Youtube code={value} />);
+  }
+
+  return (
+    <pre>
+      <code>{value}</code>
+    </pre>
+  );
+};
+
+export default Code;
