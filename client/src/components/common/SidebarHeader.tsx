c625a3b13dd562bcd5709708bbac98b01e471952
Woonjang Ahn <woonjangahn@gmail.com>
2020-07-27 21:48:19 +0900

    style: add Sidebar Layout

---
+++ client/src/components/common/SidebarHeader.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import * as S from "./assets";
+
+interface IProps {
+  src?: string;
+  title: string;
+}
+
+const SidebarHeader: React.FC<IProps> = ({src, title}) => (
+  <S.SidebarHeader>
+    <div>
+      <img src={src || S.user} alt={title} /><span>{title}</span>
+    </div>
+    <button>
+      <img src={S.more} alt="more"/>
+    </button>
+  </S.SidebarHeader>
+);
+
+export default SidebarHeader;
---
+++ client/src/components/common/SidebarLayout.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import * as S from "./assets";
+import SidebarMenu from "./SidebarMenu";
+import SidebarHeader from "./SidebarHeader";
+import {Link, NavLink} from "react-router-dom";
+
+interface IProps {
+  fluid?: boolean
+}
+
+const SidebarLayout: React.FC<IProps> = ({children, fluid = false}) => (
+  <S.SidebarLayout fluid={fluid}>
+    <nav>
+      <div>
+        <SidebarHeader src={undefined} title="Hodol"/>
+        {/* <SidebarMenu to="/me" src={S.recent} title="Recent"/>*/}
+        <NavLink to="/me" activeClassName="current">
+          <SidebarMenu src={S.slide} title="All Slides" />
+        </NavLink>
+        <NavLink to="/archive" activeClassName="current">
+          <SidebarMenu src={S.archive} title="Archive" />
+        </NavLink>
+      </div>
+      <Link to="/editor">
+        <SidebarMenu src={S.newSlides} title="New Slides"/>
+      </Link>
+    </nav>
+    <main>
+      <div>
+        {children}
+      </div>
+    </main>
+  </S.SidebarLayout>
+);
+
+export default SidebarLayout;
