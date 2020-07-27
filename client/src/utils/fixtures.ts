706e14c61326330477b8a3904d57ac3861114d93
Woonjang Ahn <woonjangahn@gmail.com>
2020-07-27 21:48:51 +0900

    chore: modify constants and fixtures

--- client/src/utils/fixtures.ts
+++ client/src/utils/fixtures.ts
@@ -1,12 +1,15 @@
 export const sampleMarkdown = {
   content: `---
-title: Hello LimeTree!
+title: Hello World!
+subtitle: Introduction to Limetree
 author: Hodol
 created_at: 2020-07-12
 ---
 
 ## hello world!
 ### this is subtitle
+
+this is paragraph
 
 --- 
 
@@ -15,6 +18,10 @@ created_at: 2020-07-12
 \`\`\`youtube
 src=wLAT6Yy7HXg
 \`\`\`
+
+---
+
+Keep going
 `,
   delimiter: "<hr>",
 };
