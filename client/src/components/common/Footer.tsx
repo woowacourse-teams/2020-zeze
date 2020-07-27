5df19879b107cd794fe167eceaa85504727e2176
Woonjang Ahn <woonjangahn@gmail.com>
2020-07-27 21:47:40 +0900

    style: modify Header, Footer, default body style

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -4,7 +4,7 @@ import * as S from "./assets";
 const Footer: React.FC = () => (
   <S.Footer>
     <div>
-    © 2020 Minguinho
+    Copyright © 2020 Minguinho.
     </div>
   </S.Footer>
 );
--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -4,9 +4,9 @@ import * as S from "./assets";
 const Header: React.FC = () => (
   <S.Header>
     <nav>
-      <div><S.HeaderIcon src={S.menu} alt="menu"/></div>
+      {/* <div><S.HeaderIcon src={S.menu} alt="menu"/></div>*/}
       <div><S.LogoIcon src={S.logo} alt="logo"/></div>
-      <div><S.HeaderIcon src={S.user} alt="user"/></div>
+      {/* <div><S.HeaderIcon src={S.user} alt="user"/></div>*/}
     </nav>
   </S.Header>
 );
