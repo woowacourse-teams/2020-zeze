226578b125e28388f1fbc9c4ae726ceff188cc0e
Woonjang Ahn <woonjangahn@gmail.com>
2020-07-28 21:30:53 +0900

    fix: resolved several bugs with theme

--- client/src/utils/markdown/metadata.ts
+++ client/src/utils/markdown/metadata.ts
@@ -10,11 +10,11 @@ const NEW_LINE_SEPARATOR = "\n";
 const KEY_VALUE_SEPARATOR = ":";
 
 const DEFAULT_PROPS: MetaProps = {
-  title: "무제",
+  title: "Untitled",
 };
 
-const parse = (value?: string) => {
-  const matches = value?.match(METADATA_REGEX);
+const parse = (text?: string) => {
+  const matches = text?.match(METADATA_REGEX);
   const metadata = matches?.[1].split(NEW_LINE_SEPARATOR)
         .map(line => {
           const [key, ...value] = line.split(KEY_VALUE_SEPARATOR);
@@ -31,17 +31,17 @@ const parse = (value?: string) => {
   if (metadata) {
     const {author, title, subtitle, createdAt} = metadata;
 
-    firstPage = `# ${title}
-## ${subtitle}
-### ${author}
-### ${createdAt}
----
-`;
+    firstPage = firstPage
+      .concat(title ? `# ${title}\n\n` : "")
+      .concat(subtitle ? `## ${subtitle}\n\n` : "")
+      .concat(author ? `### ${author}\n\n` : "")
+      .concat(createdAt ? `#### ${createdAt}\n\n` : "")
+      .concat("---\n\n");
   }
 
   return {
     metadata,
-    content: value?.replace(matches?.[0] ?? "", firstPage),
+    content: text?.replace(matches?.[0] ?? "", firstPage),
   };
 };
 
