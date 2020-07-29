76be9dc1feae2353acfb98a7dd1cc94c7975fe1b
lalize <printf@kakao.com>
2020-07-29 16:45:01 +0900

    feat: add codeblock highlight

--- client/src/components/common/markdown/renderers/Code.tsx
+++ client/src/components/common/markdown/renderers/Code.tsx
@@ -1,21 +1,24 @@
 import React from "react";
-import Language, {Youtube} from "../extensions";
+import Language, { Youtube } from "../extensions";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 interface IProps {
   language: string | undefined,
   value: string
 }
 
-const Code: React.FC<IProps> = ({language, value}) => {
+
+const Code: React.FC<IProps> = ({ language, value = "" }) => {
   switch (language) {
-  case Language.YOUTUBE:
-    return <Youtube code={value}/>;
-  default:
-    return (
-      <pre>
-        <code>{value}</code>
-      </pre>
-    );
+    case Language.YOUTUBE:
+      return <Youtube code={value} />;
+    default:
+      return (
+        <SyntaxHighlighter language={language} style={darcula}>
+          {value}
+        </SyntaxHighlighter>
+      );
   }
 };
 
