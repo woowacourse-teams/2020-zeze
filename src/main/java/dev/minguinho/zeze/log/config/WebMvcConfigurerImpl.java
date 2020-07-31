0016d3b4c8399a34223a61838ae2112beb6da634
lowoon <sangbo0229@gmail.com>
2020-07-31 17:53:45 +0900

    fix: test error logs

--- src/main/java/dev/minguinho/zeze/log/config/WebMvcConfigurerImpl.java
+++ src/main/java/dev/minguinho/zeze/log/config/WebMvcConfigurerImpl.java
@@ -1,6 +1,7 @@
 package dev.minguinho.zeze.log.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 
 import dev.minguinho.zeze.log.interceptor.LogInterceptor;
 
+@Profile("prod")
 @RequiredArgsConstructor
 @Configuration
 public class WebMvcConfigurerImpl implements WebMvcConfigurer {
