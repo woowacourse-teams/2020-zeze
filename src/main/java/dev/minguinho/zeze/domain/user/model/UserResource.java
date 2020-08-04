098aff70576eb90c535a322d94d44fc25ffae8d9
jaeju.jang <jjj0611@gmail.com>
2020-08-04 15:55:20 +0900

    feat: add @AllArgsConstructor for builder

--- src/main/java/dev/minguinho/zeze/domain/user/model/UserResource.java
+++ src/main/java/dev/minguinho/zeze/domain/user/model/UserResource.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 import dev.minguinho.zeze.domain.common.model.BaseEntity;
 
 @Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
