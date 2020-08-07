cd158230f3ddb9bbefade0e5b9754b197f6dcb17
YerinCho <forjustice9839@gmail.com>
2020-08-07 16:55:11 +0900

    refactor: remove Login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ const Home = React.lazy(() => import("./pages/Home"));
 const Me = React.lazy(() => import("./pages/Me"));
 const Editor = React.lazy(() => import("./pages/Editor"));
 const Archive = React.lazy(() => import("./pages/Archive"));
-const Login = React.lazy(() => import("./pages/Login"));
 const Error = React.lazy(() => import("./pages/Error"));
 const Callback = React.lazy(() => import("./pages/Callback"));
 
@@ -19,7 +18,6 @@ const App: React.FC = () => (
         <Route exact path="/editor" component={Editor}/>
         <Route path="/editor/:id" component={Editor}/>
         <Route path="/archive" component={Archive}/>
-        <Route path="/login" component={Login}/>
         <Route path="/callback" component={Callback}/>
         <Route component={Error}/>
       </Switch>
--- client/src/pages/Callback.tsx
+++ client/src/pages/Callback.tsx
@@ -1,34 +1,40 @@
 import React, {useEffect} from "react";
 import axios from "axios";
 import GlobalLayout from "../components/common/GlobalLayout";
+import Spinner from "../components/common/Spinner";
 
 interface props {
   location: string,
   history: string[]
 }
 
-const Callback: React.FC<props> = ( {location, history} :props ) => {
-  const githubBaseUrl: string = "/api/signin/github";
+const Callback: React.FC<props> = ({location, history}: props) => {
+  const githubBaseUrl = "/api/signin/github";
 
   useEffect(() => {
+    const getToken = async () => {
+      const code: string = location.search.toString().split("=")[1];
+      const accessToken: string = (await axios.post(githubBaseUrl, {
+        provider: "GITHUB",
+        code,
+      })).data.accessToken;
+
+      localStorage.setItem("accessToken", `bearer ${accessToken}`);
+    };
+
     getToken()
       .then(() => history.push("/me"))
-      .catch(() => history.push("/error"));
+      .catch(() => {
+        alert("login failed");
+        history.push("/");
+      });
   }, [location, history]);
-
-  const getToken = async () => {
-    const code: string = location.search.toString().split("=")[1];
-    const accessToken: string = (await axios.post(githubBaseUrl, {
-      provider: "GITHUB",
-      code,
-      })).data.accessToken;
-    localStorage.setItem("accessToken", `bearer ${accessToken}`);
-  };
 
   return (
     <GlobalLayout>
-    <div>잠시만 기다려주세요.</div>
+      <Spinner/>
     </GlobalLayout>
   );
 };
+
 export default Callback;
