1a3081088a8901a0805f86ffb98fabbdd62cd572
lalize <printf@kakao.com>
2020-08-10 16:26:41 +0900

    fix: suspense fallback error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,24 @@
-import React, {Suspense} from "react";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Spinner from "./components/common/Spinner";
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const Home = React.lazy(() => import("./pages/Home"));
 const Me = React.lazy(() => import("./pages/Me"));
 const Editor = React.lazy(() => import("./pages/Editor"));
 const Archive = React.lazy(() => import("./pages/Archive"));
-const Error = React.lazy(() => import("./pages/Error"));
 const Callback = React.lazy(() => import("./pages/Callback"));
+const Error = React.lazy(() => import("./pages/Error"));
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Suspense fallback={<Spinner/>}>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/me" component={Me}/>
-        <Route exact path="/editor" component={Editor}/>
-        <Route path="/editor/:id" component={Editor}/>
-        <Route path="/archive" component={Archive}/>
-        <Route path="/callback" component={Callback}/>
-        <Route component={Error}/>
-      </Switch>
-    </Suspense>
-  </BrowserRouter>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/me" component={Me} />
+      <Route exact path="/editor" component={Editor} />
+      <Route path="/editor/:id" component={Editor} />
+      <Route path="/archive" component={Archive} />
+      <Route path="/callback" component={Callback} />
+      <Route component={Error} />
+    </Switch>
+  </Router>
 );
 
 export default App;
