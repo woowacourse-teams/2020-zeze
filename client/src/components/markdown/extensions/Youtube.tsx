1715a8b4c8ebdb672ee0223c32a115b42a3cb2b0
Youngho Park <printf@kakao.com>
2020-08-12 22:59:09 +0900

    feat: remove youtube size properties (#127)

--- client/src/components/markdown/extensions/Youtube.tsx
+++ client/src/components/markdown/extensions/Youtube.tsx
@@ -1,49 +1,23 @@
 import React from "react";
 
-interface YoutubeProps {
-  src?: string;
-  width: string;
-  height: string;
-}
-
 const YOUTUBE_ID_REGEX = /^(?:(?:https?)?(?::\/\/)?(?:www\.)?(?:youtu\.be\/|youtube.com\/watch\?v=|youtube.com\/embed\/)?)?(\S+)$/;
-const NEW_LINE_SEPARATOR = "\n";
-const KEY_VALUE_SEPARATOR = "=";
-
-const DEFAULT_PROPS: YoutubeProps = {
-  width: "100%",
-  height: "315",
-};
 
 interface IProps {
   code: string,
 }
 
-const Youtube: React.FC<IProps> = ({code = ""}) => {
-  const {src, width, height} = code
-    .split(NEW_LINE_SEPARATOR)
-    .map(line => {
-      const [key, ...value] = line.split(KEY_VALUE_SEPARATOR);
-
-      return [key, value.join(KEY_VALUE_SEPARATOR)];
-    })
-    .reduce(
-      (previous, [key, value]) => ({
-        ...previous,
-        [key]: value,
-      }),
-      DEFAULT_PROPS,
-    );
-
-  const id = src?.match(YOUTUBE_ID_REGEX)?.[1];
+const Youtube: React.FC<IProps> = ({ code = "" }) => {
+  const id = code.match(YOUTUBE_ID_REGEX)?.[1];
 
   return id ?
     <iframe
       className="youtube"
-      title={id} width={width} height={height}
+      title={id}
+      width="100%"
+      height="315"
       src={`https://www.youtube.com/embed/${id}`} frameBorder="0"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen/> : null;
+      allowFullScreen /> : null;
 };
 
 export default Youtube;
