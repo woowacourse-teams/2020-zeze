3352f70768e9c9ad1d603c9eb89b666df695a802
jaeju.jang <jjj0611@gmail.com>
2020-08-13 13:40:14 +0900

    feat: split multi environment configuration

---
+++ db-migration/V4__Create_Table_Member_Resource.sql
@@ -0,0 +1,14 @@
+CREATE TABLE member_resource (
+   id BIGINT NOT NULL AUTO_INCREMENT,
+   created_at DATETIME(6),
+   updated_at DATETIME(6),
+   email VARCHAR(255),
+   name VARCHAR(255),
+   profile_image VARCHAR(255),
+   user_id BIGINT,
+   PRIMARY KEY (id)
+);
+
+ALTER TABLE member_resource
+    ADD CONSTRAINT uk_user_resource_user_id
+        UNIQUE (user_id);
