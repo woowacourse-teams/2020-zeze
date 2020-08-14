83e94fb1646fc31bac02c70e2a4a734fc7ae71d9
lalize <printf@kakao.com>
2020-08-14 15:20:26 +0900

    refactor: remove unnecessary code

--- client/src/components/common/FullScreenMode.tsx
+++ client/src/components/common/FullScreenMode.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "@emotion/styled";
 import fscreen from "fscreen";
 import { css, Global } from "@emotion/core";
