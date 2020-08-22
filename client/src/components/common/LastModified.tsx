c5fe41da6d53bf2ee07ff8c8bae13b33f5b1bc7c
kimhodol <woonjangahn@gmail.com>
2020-08-22 15:09:43 +0900

    feat: add last modified function

--- client/src/components/common/LastModified.tsx
+++ client/src/components/common/LastModified.tsx
@@ -6,15 +6,19 @@ interface IProps {
   at: string
 }
 
-const LastModifiedBlock = styled.div`
+interface StyleProps {
+  color?: string;
+}
+
+const LastModifiedBlock = styled.div<StyleProps>`
   text-align: right;
-  color: ${PRIMARY_GREEN};
+  color: ${props => props.color || PRIMARY_GREEN};
   font-size: 0.815rem;
-  min-width: 90px;
+  min-width: 100px;
 `;
 
-const LastModified: React.FC<IProps> = ({at}) => (
-  <LastModifiedBlock>Modified at <br/>{at}</LastModifiedBlock>
-);
+const LastModified: React.FC<IProps> = ({at}) => (at.length === 0 ?
+  <LastModifiedBlock color="#F66566">Unsaved<br/>Ctrl / Cmd + S to save</LastModifiedBlock> :
+  <LastModifiedBlock>Last modified:<br/>{at}</LastModifiedBlock>);
 
 export default LastModified;
